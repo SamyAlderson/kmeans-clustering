f51d1d05de579da5c6bab4456bae5477c79a0216
Samy Alderson <samysam35000@gmail.com>
2026-07-26 14:01:17 +0200

    Improved setup.py to use packaging 21.3 and add wheel support

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='kmeans-clustering', version='1.0', packages=['src'])
+setup(
+    name='kmeans-clustering',
+    version='1.0',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    description='General kmeans clustering library',
+    url='https://github.com/SamyAlderson/kmeans-clustering',
+    author='Samy Alderson',
+    author_email='samyalder@domain.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='kmeans clustering',
+)
